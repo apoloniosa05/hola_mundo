5b3886c23a6c02ea7b284b35faa44ceab761cded
Apolonio Salcedo Amezcua <apoloniosa05@gmail.com>
2026-02-19 12:06:49 -0600

    PRUEBA DE COMMIT PARA TALLER PROG WEB

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-         ¡Hola desde Apatzingán! El contador es {count}
+         ¡Hola saludos profe ! El contador es {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
