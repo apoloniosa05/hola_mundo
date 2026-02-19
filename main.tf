f4a668f33ac81546bb0bb70c924a5a0b371913ac
Apolonio Salcedo Amezcua <apoloniosa05@gmail.com>
2026-02-19 07:24:24 -0600

    feat: infraestructura con Terraform y actualización de UI

---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "github_token" {
+  description = "Token de acceso personal de GitHub"
+  type        = string
+  sensitive   = true
+}
+
+resource "aws_amplify_app" "hola_mundo" {
+  name       = "hola-mundo2"
+  repository = "https://github.com/apoloniosa05/hola_mundo"
+  oauth_token = var.github_token
+
+  build_spec = <<-EOT
+version: 1
+frontend:
+  phases:
+    preBuild:
+      commands:
+        - npm ci
+    build:
+      commands:
+        - npm run build
+  artifacts:
+    baseDirectory: dist
+    files:
+      - '**/*'
+  cache:
+    paths:
+      - node_modules/**/*
+EOT
+
+  custom_rule {
+    source = "</^((?!\\.).)*$/>"
+    target = "/index.html"
+    status = "200"
+  }
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.hola_mundo.id
+  branch_name = "main"
+}
